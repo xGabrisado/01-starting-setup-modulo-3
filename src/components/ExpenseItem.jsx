3452db70eb71355dd71e02886ed5dab27ec9652a
xGabrisado <madeira.gabs@gmail.com>
2023-06-23 13:37:11 -0300

    🎉  first commit, starting  ReactCourse, building firsts componnents

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem({ expense }) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={expense.date} />
+      <div className="expense-item__description">
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">${expense.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
